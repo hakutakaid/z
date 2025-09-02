eee051caaaba87c79cdb43be484a148b7e7b297b
hakutakaid <hakutakaid@users.noreply.github.com>
2025-09-02 13:52:30 +0700

    🔄 Auto Commit:  - M	fishit.lua

--- fishit.lua
+++ fishit.lua
@@ -92,7 +92,7 @@ local checkpoints = {
     {name = "Lost Isle", pos = Vector3.new(-3610.1, 2.4, -1304.6)},
     {name = "Sisyphus Statue", pos = Vector3.new(-3708.1, -135.1, -888.4)},
     {name = "Tropical Grove", pos = Vector3.new(-2003.6, 0.1, 3637.4)},
-    {name = "Checkpoint 6", pos = Vector3.new(-3195.7, 1726.8, -2617.0)},
+    {name = "Treasure Room", pos = Vector3.new(-3603.8, -282.4, -1666.3)},
 }
 
 -- Fungsi respawn (force)
